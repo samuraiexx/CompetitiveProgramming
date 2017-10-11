15da0c35d4f9028fb43306cf03be5871f141d30b
Mateus <samuraiexx@gmail.com>
2017-10-10 23:32:58 -0300

    Some questions from latin america regional

--- regional/2015/b.cpp
+++ regional/2015/b.cpp
@@ -2,55 +2,51 @@
 using namespace std;
 
 #define pb push_back
-#define mp make_pair
-#define st first
-#define nd second
 
-const int N = 300, iNF = 1e9;
-int n, src, snk, h[N], ptr[N];
-struct edge { int v, c, f;};
+const int N = 101;
 
-int n, src, snk, h[N], ptr[N];
-vector<edge> edgs;
-vector<int> g[N];
+int x, vis[N], b[N], ans;
+vector<int> adjt[N], adj[N];
 
-void add_edge(int u, int v, int c){
-    int k = edgs.size();
-    edgs.pb({v, c, 0});
-    edgs.pb({u, 0, 0});
-    g[u].push_back(k);
-    g[v].push_back(k + 1);
+bool match(int u) {
+    if(vis[u] == x) return 0;
+    vis[u] = x;
+    for(int v : adj[u])
+        if(!b[v] or match(b[v])) return b[v] = u;
+    return 0;
 }
-
-bool bfs() {
-    memsert(h, 0, sizeof h);
-    queue<int> q;
-    h[src] = 1;
-    q.push(src);
-    whle(!q.empty()){
-        int u = q.front(); q.pop();
-        for(int i : g[u]) { 
-            int v= edgs[i].v;
-            if(!h[v] and edges[i].f < edgs[i].c)
-                q.push(v), h[v] = h[u] + 1;
-        } 
-    }
-    return h[snk];
-}
-
-int dfs(int u, int flow) {
-    if(!flow or u==snk) return flow;
-    for(int &i = ptr[u]; i < g[u].size(); ++i) {
-        edge &dir = edgs[g[u][i]], &rev = edgs[g[u][i]
-
-
-
-
-typedef long long ll;
-typedef long long ld;
-
 
 int main(){
-    
+    int n, q;
+    cin >> n >> q;
+    bool can_0 = true;
+    for(int i = 1; i <= n; i++){
+        int b, ci;
+        cin >> b;
+        if(b == n) can_0 = false;
+        for(int j = 0; j < b; j++)
+            cin >> ci, adjt[i].push_back(ci);
+    }
+    for(int i = 0; i < q; i++){
+        int bs;
+        set<int> tb;
+        cin >> bs;
+        if(bs == 0){
+            if(can_0) cout << "Y\n";
+            else cout << "N\n";
+            continue;
+        }
+        x = 0, ans = 0;
+        for(int i = 1; i <= n; i++) adj[i].clear();
+        memset(b, 0, sizeof b);
+        memset(vis, 0, sizeof vis);
+        for(int i = 0, c; i < bs; i++) cin >> c, tb.insert(c);
+        for(int i = 1; i <= n; i++)
+            for(auto bi : adjt[i])
+                if(tb.count(bi)) adj[i].push_back(bi);
+        for(int i = 1; i <= n; i++) ++x, ans += match(i);
+        if(ans == tb.size()) cout << "Y\n";
+        else cout << "N\n";
+    }
    return 0;
 }
--- regional/2015/d.cpp
+++ regional/2015/d.cpp
@@ -1,29 +1,47 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-const int N = 50;
-const int M = 200;
 
-int g[M][N];
-int sum[M];
+#define px first
+#define py second
+typedef long long ll;
+const ll N = 1e5 + 1;
+
+ll n;
+pair<ll, ll> pnts[N];
+ll area[N];
+ll total;
+
+ll t_area(pair<ll, ll> p1, pair<ll, ll> p2, pair<ll, ll> p3){
+    return p1.px*p2.py + p2.px*p3.py + p3.px*p1.py
+          - p1.py*p2.px - p2.py*p3.px - p3.py*p1.px;
+}
+
+ll p_area(ll i, ll j){
+   if(i > j) swap(i, j);
+   ll mx = area[j] - area[i] - t_area(pnts[0], pnts[i], pnts[j]);
+   return max(mx, total - mx); 
+}
 
 int main(){
-    int n, m;
-    scanf("%d %d", &n, &m);
-
-    for(int i=1;i<=m;i++) for(int j=0;j<=n;j++) scanf("%d", &g[i][j]);
-
-    vector<int> c = {1, 10, 100, 1000, 10000};
-    
-    for(int i=1;i<=m;i++) for(int j=1;j<=n;j++) sum[i] += g[i][j];    
-
-    int ans = 0;
-    for(int i=1;i<=m;i++){
-        int bonus = 0;
-        for(int p : c) if(sum[i] + p - g[i][1] <= g[i][0]) bonus = p;
-        //printf("%d: %d\n", bonus, g[i][1]);
-        ans += (bonus - g[i][1] * (sum[i] <= g[i][0]));
+    cin >> n;
+    for(ll i = 0; i < n; i++)
+        cin >> pnts[i].px >> pnts[i].py;
+    for(ll i = 1; i < n; i++)
+        area[i] = area[i - 1] + t_area(pnts[0], pnts[i - 1], pnts[i]);
+    total = area[n - 1];
+    ll mx = 0;
+    for(ll i = 0; i < n; i++){
+        ll a = i + 2, b = n + i - 2;
+        while(b - a > 3)
+        {
+            ll a1 = (a + b)/2;
+            ll b1 = a1 + 1;
+            if(p_area(i, a1%n) > p_area(i, b1%n)) 
+                a = a1;
+            else b = b1;
+        }
+        mx = max(mx, min(min(p_area(i, a%n), p_area(i, (a+1)%n)), p_area(i, (a+2)%n)));
     }
-
-    printf("%d\n", ans);
+    cout << mx << ' ' << total - mx << endl;
+    return 0;
 }
